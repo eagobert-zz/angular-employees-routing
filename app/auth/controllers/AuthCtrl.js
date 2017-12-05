7a16998c2110a369de266e2b263291b5150ea4f3
eagobert <eagobert@hotmail.com>
2017-12-04 18:50:42 -0600

    Commit full user authentication files

--- app/auth/controllers/AuthCtrl.js
+++ app/auth/controllers/AuthCtrl.js
@@ -1,6 +1,20 @@
-app.controller("AuthCtrl", function($scope, AuthFactory){
-    
+angular.module("EmployeeApp")
+
+.controller("AuthCtrl", function($scope, $location, AuthFactory){
+
     $scope.auth = {}
+
+    $scope.logoutUser = function () {
+        AuthFactory.logout()
+        $location.url('/auth')
+    }
+
+    $scope.logMeIn = function () {
+        AuthFactory.authenticate($scope.auth).then(function (didLogin) {
+            $scope.login = {}
+            $location.path("/employees/list")
+        })
+    }
 
     $scope.registerUser = function(registerNewUser){
         AuthFactory.registerWithEmail(registerNewUser).then(function(didRegister){
